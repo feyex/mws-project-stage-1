ed34c6188dd0e5e52593d0ab0700ece96f23622b
feyex <feyisewaawotedu@gmail.com>
2018-08-19 04:39:17 +0100

    updated index.html

--- sw.js
+++ sw.js
@@ -10,13 +10,24 @@ this.addEventListener('install', async () => {
   const cache = await caches.open(CACHE_NAME);
   cache.addAll([
     '/',
+    '/index.html',
     '/css/styles.css',
     '/js/main.js',
     '/js/dbhelper.js',
     '/js/main.js',
     '/data/restaurants.json',
     '/js/restaurant_info.js',
-    '/img'
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg',
+    'restaurant.html?id=1', 'restaurant.html?id=2', 'restaurant.html?id=3', 'restaurant.html?id=4', 'restaurant.html?id=5', 'restaurant.html?id=6', 'restaurant.html?id=7', 'restaurant.html?id=8', 'restaurant.html?id=9', 'restaurant.html?id=10'
   ]).then(() => {
     console.log('WORKER: install completed');
   })
@@ -31,3 +42,17 @@ self.addEventListener('fetch', function(event) {
     })
   );
  });
+
+//  if(url.startsWith('localhost:') && (url.includes('tiles.mapbox.com') || url.includes('api.mapbox.com'))) {
+//     event.respondWith(
+//       caches.match(event.request).then(function(resp) {
+//         return resp || fetch(event.request).then(function(response) {
+//           var cacheResponse = response.clone();
+//           caches.open('mapbox').then(function(cache) {
+//             cache.put(event.request, cacheResponse);
+//           });
+//           return response;
+//         });
+//       })
+//     );
+//   }
