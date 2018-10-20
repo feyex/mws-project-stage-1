d980f9f528e8697ea5e65751600abe7b8e5afd83
feyex <feyisewaawotedu@gmail.com>
2018-10-20 22:32:48 +0100

    docs:updated git ignored and changed file structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,36 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package-lock.json'),
+    pkg: grunt.file.readJSON('package-lock.json'),
+  //CLEAN: To delete everything from the image file
+      clean:{
+        dev: {
+          src: ['dist/*'],
+        }
+      },
+  //COPY: To move css, js, and unprocessed image files
+      copy: {
+        dev: {
+          files: [
+            {expand: true, cwd: 'app/', src:['sw.js'], dest: 'dist/'},
+            {expand: true, cwd: 'app/', src:['css/*'], dest: 'dist/'},
+            {expand: true, cwd: 'app/', src:['js/*'], dest: 'dist/'},
+            {expand: true, cwd: 'app/', src:['img/fixed/*'], dest: 'dist/'},
+          ]
+        }
+      },
+  //STRING-REPLACE: To move my  MAP BOX API KEY from my html file to a folder that wont be accessible on github
+      'string-replace':{
+        dist: {
+          files:[{expand: true, cwd: 'app/', src:['*.html'], dest: 'dist/'}],
+          options: {
+            replacements: [{
+              pattern: '<API_KEY_HERE>',
+              replaceent: '<%= grunt.file.read("GM_API_KEY") %>'
+            }]
+         }
+        }
+      },
+  //IMAGEMIN: To minify my images.
         imagemin: {
             jpg: {
               options: {
@@ -9,9 +39,9 @@ module.exports = function(grunt) {
               files: [
                 {
                   expand: true,
-                  cwd: 'img/',
+                  cwd: 'app/img/',
                   src: ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg'],
-                  dest: 'imgs/',
+                  dest: 'dist/img/',
                   ext: '.jpg'
                 }
               ]
@@ -19,6 +49,14 @@ module.exports = function(grunt) {
         }
 	});
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-string-replace');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-}
+    grunt.registerTask('quick', ['copy', 'string-replace']);
+
+  grunt.registerTask('default', ['clean', 'copy', 'string-replace',
+    'contrib-imagemin']);
+
+};
