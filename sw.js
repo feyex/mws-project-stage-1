de6cd421c39644158cc7e18cefec638e0d0c9c42
feyex <feyisewaawotedu@gmail.com>
2018-10-23 21:17:53 +0100

    fix:fetch request syntax error

--- sw.js
+++ sw.js
@@ -33,120 +33,15 @@ this.addEventListener('install', async () => {
   })
 });
 
-// self.addEventListener('fetch', function(event) {
-//   console.log(event.request.url);
+
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
  
-//   event.respondWith(
-//     caches.match(event.request).then(function(response) {
-//       return response || fetch(event.request);
-//     })
-//   );
-//  });
-
-self.addEventListener('fetch', event => {
   event.respondWith(
-    // Add cache.put to cache images on each fetch
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).then(fetchResponse => {
-        return caches.open(staticCacheName).then(cache => {
-          // filter out browser-sync resources otherwise it will err
-          if (!fetchResponse.url.includes('browser-sync')) { // prevent err
-            cache.put(event.request, fetchResponse.clone());
-          }
-          return fetchResponse;
-        });
-      });
-    }).catch(error => new Response(error));
-  );
-});
-
-
-//Setting up of a constant called dbPromise which is asssigned the results of idb.open operation.
- const dbPromise = idb.open('restaurant', 1, upgradeDB => {
-   switch (upgradeDB.oldVersion) {
-     case 0: 
-      upgradeDB.createObjectStore('restaurants');
-   }
- })
-
-//  const dbPromise = idb.open('currency', 1, function (upgradeDb) {
-//   let keyValStore = upgradeDb.createObjectStore('keyval', {
-//     keyPath: 'id'
-//   });
-// });
-
-//indexed db  object with get and set methods
-const idbKeyVal = {
-  get(key) {
-    return dbPromise.then(db => {
-      return db
-        .transaction('restaurants')
-        .objectStore('restaurants')
-        .get(key);
-    });
-  },
-  set(key, val) {
-    return dbPromise.then(db => {
-      const tx =db.transaction('restaurants', 'readwrite');
-      tx.objectStore('restaurants').put(val, key);
-      return tx.complete;
-    });
-  }
-};
-
-
-self.addEventListener('fetch', event => {
-  const request = event.request;
-  const requestUrl = new URL(request.url);
-
-  if (requestUrl.port === '1337') {
-    event.respondWith(idbResponse(request));
-  }
-  else {
-    event.respondWith(cacheResponse(request));
-  }
-});
-
-
-function cacheResponse(request) {
-  // match request...
-  return caches.match(request)
-    .then(response => {
-    // return matched response OR if no match then
-    // fetch, open cache, cache.put response.clone, return response
-      return 
-        response || 
-        fetch(request).then(fetchResponse => {
-          return caches.open(staticCacheName).then(cache => {
-            // filter out browser-sync resources otherwise it will err
-            if (!fetchResponse.url.includes('browser-sync')) { // prevent err
-              cache.put(request, fetchResponse.clone()); // put clone in cache
-            }
-            return fetchResponse; // send original back to browser
-          });
-      });
-  }).catch(error => new Response(error));
-}
-
-
-function idbResponse(request) {
-  return idbKeyVal.get('restaurants')
-    .then(restaurants => {
-      return (
-        restaurants ||
-        fetch(request)
-          .then(response => response.json())
-          .then(json => {
-            idbKeyVal.set('restaurants', json);
-            return json;
-          })
-      );
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
-    .then(response => new Response(JSON.stringify(response)))
-    .catch(error => {
-      return new Response(error, {
-        status: 404,
-        statusText: 'my bad request'
-      });
-    });
-}
+  );
+ });
+
